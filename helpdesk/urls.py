dd40a4df29e7986c42fc345a43a24285a5507d86
Radek lejba <radeklejba@gmail.com>
2019-03-13 22:42:43 +0100

    Added static folder, login page and sass main file

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import include, path
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from helpdesk.views import IndexView
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^admin/', admin.site.urls),
+    path(r'', IndexView.as_view(), name='index'),
+    path(r'', include('django.contrib.auth.urls')),
+    path(r'admin/', admin.site.urls),
 ]
